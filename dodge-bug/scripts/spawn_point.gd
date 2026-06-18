48feecb95b0a0c2058891e863a1008a669cb8e04
Muadz-coder <24433@burnside.school.nz>
2026-06-18 14:39:25 +1200

    added little bit

--- dodge-bug/scripts/spawn_point.gd
+++ dodge-bug/scripts/spawn_point.gd
@@ -2,11 +2,14 @@ extends Node2D
 
 @export var spike_scene: PackedScene
 @export var warning_scene: PackedScene
+@export var fruit_scene: PackedScene
 
 @onready var spawn_points = $SpawnPoints.get_children()
+@onready var spawn_points2 = $SpawnPoints2.get_children()
 @onready var spawn_point = $SpawnPoint
 @onready var spike_timer = $SpawnTimer
 @onready var warning_timer = $SpawnTimerW
+
 
 var pending_spike_positions: Array = []
 
@@ -22,6 +25,8 @@ func _ready():
 func get_random_spawn_point():
 	return spawn_points[randi() % spawn_points.size()]
 
+func get_random_spawn_points2():
+	return spawn_points2[randi() % spawn_points2.size()]
 
 func _on_spawn_timer_w_timeout() -> void:
 	pending_spike_positions.clear()
@@ -40,3 +45,35 @@ func _on_spawn_timer_w_timeout() -> void:
 		var spike = spike_scene.instantiate()
 		spike.global_position = pos
 		add_child(spike)
+
+
+
+var occupied_points := {}
+
+func _on_spawn_timer_f_timeout() -> void:
+	var available := []
+
+	# Collect all spawn points that are NOT occupied
+	for sp in spawn_points2:
+		if not occupied_points.has(sp):
+			available.append(sp)
+
+	# If no free points, stop
+	if available.is_empty():
+		return
+
+	# Spawn up to 3 fruits on free points
+	for i in range(min(3, available.size())):
+		var sp = available.pick_random()
+		available.erase(sp)
+
+		var fruit = fruit_scene.instantiate()
+		fruit.global_position = sp.global_position
+		add_child(fruit)
+
+		# Mark spawn point as occupied
+		occupied_points[sp] = fruit
+
+		# Free the point when fruit disappears
+		fruit.tree_exited.connect(func():
+			occupied_points.erase(sp))
